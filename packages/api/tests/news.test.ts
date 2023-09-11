a20cc8712301d3177bab035c4191811da60bb175
aadv1k <aadv1k@outlook.com>
2023-09-11 16:13:32 +0530

    Add failing test

--- packages/api/tests/news.test.ts
+++ packages/api/tests/news.test.ts
@@ -2,7 +2,14 @@ import request from 'supertest';
 import app from '../src/server';
 
 describe('Tests for /v1/news', () => {
-  test.todo('Should return news data without API key');
+  test('Should return news data without API key', async () => {
+      const response = await request(app)
+          .get('/v1/news')
+
+      const data = response.body;
+      expect(response.status).toBe(201);
+  });
+
   test.todo('Should return news data with valid API key');
   test.todo('Should return rate-limit error without API key');
   test.todo('Should return news data with invalid API key');
@@ -10,5 +17,4 @@ describe('Tests for /v1/news', () => {
   test.todo('Should return news data with valid API key and specific bias');
   test.todo('Should return an error for an invalid bias parameter');
   test.todo('Should handle pagination correctly');
-  test.todo('Should return a 404 error for non-existent routes');
 });
